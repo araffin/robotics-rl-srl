ea04a71c719f47ad776e75e95ebd1069242660f6
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-03-06 13:31:55 +0100

    Add MLP policy for a2c

--- rl_baselines/a2c.py
+++ rl_baselines/a2c.py
@@ -1,6 +1,10 @@
 from baselines.a2c.a2c import *
 from baselines.common.vec_env.vec_frame_stack import VecFrameStack
-from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy, MlpPolicy
+from baselines.common.vec_env.vec_normalize import VecNormalize
+from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy
+from baselines.a2c.utils import fc
+from baselines.common.distributions import make_pdtype
+
 from baselines import logger
 
 import environments.kuka_button_gym_env as kuka_env
@@ -8,9 +12,110 @@ from pytorch_agents.envs import make_env
 from rl_baselines.utils import createTensorflowSession
 
 
+class Runner(object):
+    def __init__(self, env, model, nsteps=5, gamma=0.99):
+        self.env = env
+        self.model = model
+        nenv = env.num_envs
+        if len(env.observation_space.shape) > 1:
+            nh, nw, nc = env.observation_space.shape
+            self.batch_ob_shape = (nenv * nsteps, nh, nw, nc)
+            self.dtype = np.uint8
+            self.obs = np.zeros((nenv, nh, nw, nc), dtype=self.dtype)
+            self.nc = nc
+        else:
+            obs_dim = env.observation_space.shape[0]
+            self.batch_ob_shape = (nenv * nsteps, obs_dim)
+            self.dtype = np.float32
+            self.obs = np.zeros((nenv, obs_dim), dtype=self.dtype)
+
+        obs = env.reset()
+        self.gamma = gamma
+        self.nsteps = nsteps
+        self.states = model.initial_state
+        self.dones = [False for _ in range(nenv)]
+
+    def run(self):
+        mb_obs, mb_rewards, mb_actions, mb_values, mb_dones = [], [], [], [], []
+        mb_states = self.states
+        for n in range(self.nsteps):
+            actions, values, states, _ = self.model.step(self.obs, self.states, self.dones)
+            mb_obs.append(np.copy(self.obs))
+            mb_actions.append(actions)
+            mb_values.append(values)
+            mb_dones.append(self.dones)
+            obs, rewards, dones, _ = self.env.step(actions)
+            self.states = states
+            self.dones = dones
+            for k, done in enumerate(dones):
+                if done:
+                    self.obs[k] = self.obs[k] * 0
+            self.obs = obs
+            mb_rewards.append(rewards)
+        mb_dones.append(self.dones)
+        # batch of steps to batch of rollouts
+        mb_obs = np.asarray(mb_obs, dtype=self.dtype).swapaxes(1, 0).reshape(self.batch_ob_shape)
+        mb_rewards = np.asarray(mb_rewards, dtype=np.float32).swapaxes(1, 0)
+        mb_actions = np.asarray(mb_actions, dtype=np.int32).swapaxes(1, 0)
+        mb_values = np.asarray(mb_values, dtype=np.float32).swapaxes(1, 0)
+        mb_dones = np.asarray(mb_dones, dtype=np.bool).swapaxes(1, 0)
+        mb_masks = mb_dones[:, :-1]
+        mb_dones = mb_dones[:, 1:]
+        last_values = self.model.value(self.obs, self.states, self.dones).tolist()
+        # discount/bootstrap off value fn
+        for n, (rewards, dones, value) in enumerate(zip(mb_rewards, mb_dones, last_values)):
+            rewards = rewards.tolist()
+            dones = dones.tolist()
+            if dones[-1] == 0:
+                rewards = discount_with_dones(rewards + [value], dones + [0], self.gamma)[:-1]
+            else:
+                rewards = discount_with_dones(rewards, dones, self.gamma)
+            mb_rewards[n] = rewards
+        mb_rewards = mb_rewards.flatten()
+        mb_actions = mb_actions.flatten()
+        mb_values = mb_values.flatten()
+        mb_masks = mb_masks.flatten()
+        return mb_obs, mb_states, mb_rewards, mb_masks, mb_actions, mb_values
+
+
+# Redefine MLP policy to support srl models
+class MlpPolicy(object):
+    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, reuse=False):
+        ob_shape = (nbatch,) + ob_space.shape
+        actdim = ac_space.n
+        X = tf.placeholder(tf.float32, ob_shape, name='Ob')  # obs
+        with tf.variable_scope("model", reuse=reuse):
+            activ = tf.tanh
+            h1 = activ(fc(X, 'pi_fc1', nh=64, init_scale=np.sqrt(2)))
+            h2 = activ(fc(h1, 'pi_fc2', nh=64, init_scale=np.sqrt(2)))
+            pi = fc(h2, 'pi', actdim, init_scale=0.01)
+            h1 = activ(fc(X, 'vf_fc1', nh=64, init_scale=np.sqrt(2)))
+            h2 = activ(fc(h1, 'vf_fc2', nh=64, init_scale=np.sqrt(2)))
+            vf = fc(h2, 'vf', 1)[:, 0]
+
+        self.pdtype = make_pdtype(ac_space)
+        self.pd = self.pdtype.pdfromflat(pi)
+
+        a0 = self.pd.sample()
+        neglogp0 = self.pd.neglogp(a0)
+        self.initial_state = None
+
+        def step(ob, *_args, **_kwargs):
+            a, v, neglogp = sess.run([a0, vf, neglogp0], {X: ob})
+            return a, v, self.initial_state, neglogp
+
+        def value(ob, *_args, **_kwargs):
+            return sess.run(vf, {X: ob})
+
+        self.X = X
+        self.pi = pi
+        self.vf = vf
+        self.step = step
+        self.value = value
+
+
 def learn(policy, env, seed=0, nsteps=5, total_timesteps=int(1e6), vf_coef=0.5, ent_coef=0.01, max_grad_norm=0.5,
           lr=7e-4, lrschedule='linear', epsilon=1e-5, alpha=0.99, gamma=0.99, log_interval=100, callback=None):
-
     tf.reset_default_graph()
     createTensorflowSession()
 
@@ -75,14 +180,17 @@ def main(args, callback):
     """
 
     if args.srl_model != "":
-        raise NotImplementedError("RL on SRL not supported for a2c")
-        # print("Using MLP policy because working on state representation")
-        # args.policy = "mlp"
+        print("Using MLP policy because working on state representation")
+        args.policy = "mlp"
 
     envs = [make_env(args.env, args.seed, i, args.log_dir, pytorch=False)
             for i in range(args.num_cpu)]
 
     envs = SubprocVecEnv(envs)
+    if args.srl_model == "ground_truth":
+        # TODO: save running average
+        envs = VecNormalize(envs)
+
     envs = VecFrameStack(envs, args.num_stack)
     logger.configure()
     learn(args.policy, envs, total_timesteps=args.num_timesteps, seed=args.seed,
