02e5d95a2757f5110c311d43d18881df7f97f9a3
Kalifou René TR <kalifou@users.noreply.github.com>
2018-03-28 17:29:14 +0200

    Update test_env.py

--- environments/test_env.py
+++ environments/test_env.py
@@ -6,7 +6,7 @@ import environments.kuka_button_gym_env as kuka_env
 
 
 kuka_env.MAX_DISTANCE = 0.65
-kuka_env.RECORD_DATA = True
+kuka_env.RECORD_DATA = False
 
 env = kuka_env.KukaButtonGymEnv(renders=False, is_discrete=True, multi_view=False, name="kuka_test_dual_cam")
 # env.num_envs = 1
