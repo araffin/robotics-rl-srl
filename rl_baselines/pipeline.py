ab60848abac0a9b7c1806224675d5a22abda9d30
Ashley Hill <ashley.hill.1993@gmail.com>
2018-04-06 16:33:01 +0200

    cleanup and commenting

--- rl_baselines/pipeline.py
+++ rl_baselines/pipeline.py
@@ -4,29 +4,32 @@ baseline benchmark script for openAI RL Baselines
 import os
 import argparse
 import subprocess
-from pprint import pprint
 
 from srl_priors.utils import printGreen, printRed
 
-SEEDS = [0,2,4,6] # the seeds used in trainning the baseline.
+SEEDS = [0, 2, 4, 6]  # the seeds used in trainning the baseline.
+
 
 def main():
     parser = argparse.ArgumentParser(description="OpenAI RL Baselines Benchmark")
-    parser.add_argument('--algo', default='deepq', 
+    parser.add_argument('--algo', default='deepq',
                         choices=['acer', 'deepq', 'a2c', 'ppo2', 'random_search', 'random_agent', 'ddpg'],
                         help='OpenAI baseline to use', type=str)
     parser.add_argument('--env', type=str, help='environment ID', default='all',
-                        choices=["all","KukaButtonGymEnv-v0"])
+                        choices=["all", "KukaButtonGymEnv-v0"])
     parser.add_argument('--srl-model', type=str, default='all',
-                        choices=['all', "autoencoder", "ground_truth", "srl_priors", "supervised", "pca", "vae", "joints", "joints_position", "raw_pixels"],
+                        choices=['all', "autoencoder", "ground_truth", "srl_priors", "supervised", "pca", "vae",
+                                 "joints", "joints_position", "raw_pixels"],
                         help='SRL model to use')
-    parser.add_argument('--num-timesteps', type=int, default=int(1e6*1.1))
-    parser.add_argument('-v', '--verbose', action='store_true', default=False)
+    parser.add_argument('--num-timesteps', type=int, default=int(1e6 * 1.1), 
+                        help='number of timesteps the baseline should run')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Display baseline STDOUT')
 
     args, train_args = parser.parse_known_args()
 
     if args.srl_model == "all":
-        models = ["autoencoder", "ground_truth", "srl_priors", "supervised", "pca", "vae", "joints", "joints_position", "raw_pixels"]
+        models = ["autoencoder", "ground_truth", "srl_priors", "supervised", "pca", "vae", "joints", "joints_position",
+                  "raw_pixels"]
     else:
         models = [args.srl_model]
 
@@ -40,7 +43,6 @@ def main():
         stdout = None
     else:
         stdout = open(os.devnull, 'w')
-
 
     printGreen("\nRunning {} benchmarks...".format(args.algo))
     print("\nSRL-Models:\t{}".format(models))
@@ -57,7 +59,8 @@ def main():
                 if model != "raw_pixels":
                     # raw_pixels is when --srl-model is left as default
                     train_args.extend(['--srl-model', model])
-                train_args.extend(['--seed', str(SEEDS[seed_idx]), '--algo', args.algo, '--env', env, '--num-timesteps', str(args.num_timesteps)])
+                train_args.extend(['--seed', str(SEEDS[seed_idx]), '--algo', args.algo, '--env', env, '--num-timesteps',
+                                   str(args.num_timesteps)])
 
                 ok = subprocess.call(['python', '-m', 'rl_baselines.train'] + train_args, stdout=stdout)
 
@@ -65,6 +68,7 @@ def main():
                     printRed("An error occured, error code: {}".format(ok))
                     # throw the error down to the terminal
                     return ok
+
 
 if __name__ == '__main__':
     main()
