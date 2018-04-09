0c46eb005a5697240adc41ab00028a16b6cd418b
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-04-09 10:52:40 +0200

    Update plot limits

--- replay/aggregate_plots.py
+++ replay/aggregate_plots.py
@@ -24,7 +24,7 @@ darkcolors = colors[1::2]
 
 # y-limits for the plot
 Y_LIM_SPARSE_REWARD = [-3, 6]
-Y_LIM_SHAPED_REWARD = [-220, -130]
+Y_LIM_SHAPED_REWARD = [-90, -35]
 
 
 def loadEpisodesData(folder):
