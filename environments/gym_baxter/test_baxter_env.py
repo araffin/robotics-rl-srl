9db53d99e06485a4561e68d9cd4f35850a030f6d
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-04-09 19:02:45 +0200

    Bug fixes for baxter env

--- environments/gym_baxter/test_baxter_env.py
+++ environments/gym_baxter/test_baxter_env.py
@@ -1,31 +1,31 @@
 import time
 
-from environments.gym_baxter.baxter_env import BaxterEnv
+import environments.gym_baxter.baxter_env as baxter_env
 
-BaxterEnv.RECORD_DATA = True
+baxter_env.RECORD_DATA = True
 # Reduce max distance to have more negative rewards for srl
-BaxterEnv.MAX_DISTANCE = 0.65
+baxter_env.MAX_DISTANCE = 0.30
 
-env = BaxterEnv(renders=True, is_discrete=True, log_folder="baxter_recorder_data_log")
+env = baxter_env.BaxterEnv(renders=False, is_discrete=True, log_folder="baxter_test")
 timesteps = 500
-episodes = 50
-env.seed(0)
+episodes = 400
+env.seed(1)
 i = 0
 
 print('Starting episodes...')
 start_time = time.time()
-for _ in range(episodes):
-    observation = env.reset()
-    for t in range(timesteps):
-        try:
-            action = env.action_space.sample()
-            observation, reward, done, info = env.step(action)
-            env.render()  # render() requires first the observation to be obtained
-            if done:
-                print("Episode finished after {} timesteps".format(t + 1))
-                break
-            i += 1
-        except KeyboardInterrupt:
-            pass
+try:
+    for _ in range(episodes):
+        observation = env.reset()
+        for t in range(timesteps):
+                action = env.action_space.sample()
+                observation, reward, done, info = env.step(action)
+                env.render()  # render() requires first the observation to be obtained
+                if done:
+                    print("Episode finished after {} timesteps".format(t + 1))
+                    break
+                i += 1
+except KeyboardInterrupt:
+    pass
 env.closeServerConnection()
 print("Avg. frame rate: {:.2f} FPS".format(i / (time.time() - start_time)))
