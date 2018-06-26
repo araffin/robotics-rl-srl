param tweaking + fixed softmax

rl_baselines/utils.py
@@ -348,6 +348,6 @@ def loadRunningAverage(envs, load_path_normalise=None):
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
+    e_x = np.exp(x.T - np.max(x.T, axis=0))
+    return (e_x / e_x.sum(axis=0)).T
 