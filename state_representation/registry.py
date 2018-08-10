db9b5dc5d1525f07e9547a8dee8e15b56029def3
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-08-10 11:54:41 +0200

    Add new models

--- state_representation/registry.py
+++ state_representation/registry.py
@@ -3,17 +3,24 @@ from environments.kuka_gym.kuka_button_gym_env import KukaButtonGymEnv
 
 # format NAME: (SRLType, LIMITED_TO_ENV)
 registered_srl = {
-    "raw_pixels":      (SRLType.ENVIRONMENT, None),
-    "ground_truth":    (SRLType.ENVIRONMENT, None),
-    "joints":          (SRLType.ENVIRONMENT, [KukaButtonGymEnv]),
+    "raw_pixels": (SRLType.ENVIRONMENT, None),
+    "ground_truth": (SRLType.ENVIRONMENT, None),
+    "joints": (SRLType.ENVIRONMENT, [KukaButtonGymEnv]),
     "joints_position": (SRLType.ENVIRONMENT, [KukaButtonGymEnv]),
-    "robotic_priors":  (SRLType.SRL, None),
-    "inverse":         (SRLType.SRL, None),
-    "forward":         (SRLType.SRL, None),
-    "multi_view_srl":  (SRLType.SRL, None),
+    "robotic_priors": (SRLType.SRL, None),
+    "inverse": (SRLType.SRL, None),
+    "forward": (SRLType.SRL, None),
+    "multi_view_srl": (SRLType.SRL, None),
     "srl_combination": (SRLType.SRL, None),
-    "supervised":      (SRLType.SRL, None),
-    "autoencoder":     (SRLType.SRL, None),
-    "vae":             (SRLType.SRL, None),
-    "pca":             (SRLType.SRL, None)
+    "supervised": (SRLType.SRL, None),
+    "autoencoder": (SRLType.SRL, None),
+    "autoencoder_inverse": (SRLType.SRL, None),
+    "autoencoder_reward": (SRLType.SRL, None),
+    "random": (SRLType.SRL, None),
+    "random_inverse": (SRLType.SRL, None),
+    "reward_inverse": (SRLType.SRL, None),
+    "srl_splits": (SRLType.SRL, None),
+    "reward": (SRLType.SRL, None),
+    "vae": (SRLType.SRL, None),
+    "pca": (SRLType.SRL, None)
 }
