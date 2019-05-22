fix for on-policy data-gen: normalizing obs

environments/dataset_generator.py
@@ -18,9 +18,10 @@ from torch.autograd import Variable
 
 from environments import ThreadingType
 from environments.registry import registered_env
+from environments.utils import makeEnv
 from real_robots.constants import *
 from replay.enjoy_baselines import createEnv, loadConfigAndSetup
-from rl_baselines.utils import MultiprocessSRLModel
+from rl_baselines.utils import MultiprocessSRLModel, loadRunningAverage
 from srl_zoo.utils import printRed, printYellow
 from srl_zoo.preprocessing.utils import deNormalize
 from state_representation.models import loadSRLModel, getSRLDim
@@ -39,7 +40,7 @@ def latestPath(path):
     :param path: path to the log folder (defined in srl_model.yaml) (str)
     :return: path to latest learned model in the same dataset folder (str)
     """
-    return max([path + d for d in os.listdir(path) if os.path.isdir(path + "/" + d)],key=os.path.getmtime) + '/'
+    return max([path + d for d in os.listdir(path) if os.path.isdir(path + "/" + d)], key=os.path.getmtime) + '/'
 
 
 def walkerPath():
@@ -136,17 +137,34 @@ def env_thread(args, thread_num, partition=True):
         env_kwargs["random_target"] = env_kwargs_extra.get("random_target", False)
         env_kwargs["use_srl"] = env_kwargs_extra.get("use_srl", False)
 
+        # TODO REFACTOR
+        env_kwargs["simple_continual_target"] = env_kwargs_extra.get("simple_continual_target", False)
+        env_kwargs["circular_continual_move"] = env_kwargs_extra.get("circular_continual_move", False)
+        env_kwargs["square_continual_move"] = env_kwargs_extra.get("square_continual_move", False)
+        env_kwargs["eight_continual_move"] = env_kwargs_extra.get("eight_continual_move", False)
+
         eps = 0.2
         env_kwargs["state_init_override"] = np.array([MIN_X + eps, MAX_X - eps]) \
             if args.run_policy == 'walker' else None
         if env_kwargs["use_srl"]:
             env_kwargs["srl_model_path"] = env_kwargs_extra.get("srl_model_path", None)
             env_kwargs["state_dim"] = getSRLDim(env_kwargs_extra.get("srl_model_path", None))
-            srl_model = MultiprocessSRLModel(args.num_cpu, args.env, env_kwargs)
+            srl_model = MultiprocessSRLModel(num_cpu=args.num_cpu, env_id=args.env, env_kwargs=env_kwargs)
             env_kwargs["srl_pipe"] = srl_model.pipe
 
     env_class = registered_env[args.env][0]
     env = env_class(**env_kwargs)
+
+    if env_kwargs['srl_model'] != "raw_pixels":
+        # TODO: Remove env duplication
+        #  This is a dirty trick to normalize the obs.
+        #  So for as we override SRL environment functions (step, reset) for on-policy generation & generative replay
+        #  using stable-baselines' normalisation wrappers (step & reset) breaks...
+        env_norm = [makeEnv(args.env, args.seed, i, args.log_dir, allow_early_resets=False, env_kwargs=env_kwargs)
+                    for i in range(args.num_cpu)]
+        env_norm = DummyVecEnv(env_norm)
+        env_norm = VecNormalize(env_norm, norm_obs=True, norm_reward=False)
+        env_norm = loadRunningAverage(env_norm, load_path_normalise=args.log_custom_policy)
     using_real_omnibot = args.env == "OmnirobotEnv-v0" and USING_OMNIROBOT
 
     walker_path = None
@@ -155,7 +173,6 @@ def env_thread(args, thread_num, partition=True):
     kwargs_reset, kwargs_step = {}, {}
 
     if args.run_policy in ['custom', 'ppo2', 'walker']:
-
         # Additional env when using a trained agent to generate data
         train_env = vecEnv(env_kwargs, env_class)
 
@@ -164,7 +181,7 @@ def env_thread(args, thread_num, partition=True):
         else:
             _, _, algo_args = createEnv(args, train_args, algo_name, algo_class, env_kwargs)
             tf.reset_default_graph()
-            set_global_seeds(args.seed)
+            set_global_seeds(args.seed % 2 ^ 32)
             printYellow("Compiling Policy function....")
             model = algo_class.load(load_path, args=algo_args)
             if args.run_policy == 'walker':
@@ -184,9 +201,8 @@ def env_thread(args, thread_num, partition=True):
         # seed + position in this slice + size of slice (with reminder if uneven partitions)
         seed = args.seed + i_episode + args.num_episode // args.num_cpu * thread_num + \
                (thread_num if thread_num <= args.num_episode % args.num_cpu else args.num_episode % args.num_cpu)
-
+        seed = seed % 2 ^ 32
         if not (args.run_policy in ['custom', 'walker']):
-            seed = seed % 2^32
             env.seed(seed)
             env.action_space.seed(seed)  # this is for the sample() function from gym.space
 
@@ -217,6 +233,7 @@ def env_thread(args, thread_num, partition=True):
 
             # Custom pre-trained Policy (SRL or End-to-End)
             elif args.run_policy in['custom', 'walker']:
+                obs =  env_norm._normalize_observation(obs)
                 action = [model.getAction(obs, done)]
                 action_proba = model.getActionProba(obs, done)
                 if args.run_policy == 'walker':
@@ -245,8 +262,8 @@ def env_thread(args, thread_num, partition=True):
             action_to_step = action[0]
 
             kwargs_step = {k: v for (k, v) in [("generated_observation", generated_obs),
-                                          ("action_proba", action_proba),
-                                          ("action_grid_walker", action_walker)] if v is not None}
+                                               ("action_proba", action_proba),
+                                               ("action_grid_walker", action_walker)] if v is not None}
 
             obs, _, done, _ = env.step(action_to_step, **kwargs_step)
 